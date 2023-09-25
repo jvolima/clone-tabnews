e31c6d54b5e26af346fbe2516c2242045e5f40ae
João Vitor <jvolima2004@gmail.com>
2023-09-25 13:40:15 -0300

    feat: add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ function Home() {
     <main>
       <h1>Objetivos da plataforma tabufscar.com.br</h1>
       <p>
-        Promover discussões sobre os mais variados assuntos que abrangem
-        a Universidade Federal de São Carlos, como por exemplo: falar sobre
-        a comida do RU, reclamar das provas e trabalhos, divulgar vagas de
-        estágio e projetos de extensão, entre outros.
+        Promover discussões sobre os mais variados assuntos que abrangem a
+        Universidade Federal de São Carlos, como por exemplo: falar sobre a
+        comida do RU, reclamar das provas e trabalhos, divulgar vagas de estágio
+        e projetos de extensão, entre outros.
       </p>
     </main>
   );
