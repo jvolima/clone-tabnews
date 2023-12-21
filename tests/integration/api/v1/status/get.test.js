20b8fc77bcc5f0f54c254d5fb5c1a84825515bed
João Vitor <jvolima2004@gmail.com>
2023-12-21 12:03:04 -0300

    feat: finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,29 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody).toMatchObject({
+    updated_at: expect.any(String),
+    dependencies: {
+      database: {
+        version: expect.any(String),
+        max_connections: expect.any(Number),
+        opened_connections: expect.any(Number),
+      },
+    },
+  });
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(
+    responseBody.dependencies.database.max_connections,
+  ).toBeGreaterThanOrEqual(
+    responseBody.dependencies.database.opened_connections,
+  );
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
