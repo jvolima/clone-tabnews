d6a625e302134a8caa0ee9f4fec0ec8d6a2ac846
João Vitor <jvolima2004@gmail.com>
2024-05-01 18:59:35 -0300

    feat: add `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,19 @@
+import database from "infra/database";
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+beforeAll(cleanDatabase);
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const pgmigrations = await database.query("select * from pgmigrations;");
+  expect(pgmigrations.rowCount).toBe(0);
+});
