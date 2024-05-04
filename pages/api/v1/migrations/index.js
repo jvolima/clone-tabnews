78d46ceadf92afcd1740e03ed22d3b536ef57bc4
João Vitor <jvolima2004@gmail.com>
2024-05-04 11:36:01 -0300

    fix: `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,7 +3,14 @@ import path from "path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  let dbClient
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+
+  let dbClient;
   try {
     dbClient = await database.getNewClient();
     const defaultMigrationOptions = {
@@ -14,27 +21,28 @@ export default async function migrations(request, response) {
       verbose: true,
       migrationsTable: "pgmigrations",
     };
-  
+
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-  
+
       return response.status(200).json(pendingMigrations);
     }
-  
+
     if (request.method === "POST") {
       const migratedMigrations = await migrationRunner({
         ...defaultMigrationOptions,
         dryRun: false,
       });
-  
+
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
-  
+
       return response.status(200).json(migratedMigrations);
     }
-  
-    return response.status(405).end();
+  } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
   }
