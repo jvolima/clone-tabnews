59d26bfb13a0536ef07905947236587934a65afb
João Vitor <jvolima2004@gmail.com>
2025-05-07 22:36:22 -0300

    feat: add migration to create `users` table

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,27 @@
+import orchestrator from "tests/orchestrator.js";
+import database from "infra/database";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
+        values: ["jvolima", "jvolima2004@gmail.com", "senha123"],
+      });
+
+      const users = await database.query("SELECT * FROM users;");
+      console.log(users.rows);
+
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+    });
+  });
+});
